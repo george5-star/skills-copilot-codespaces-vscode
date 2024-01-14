8e3856cda97c8570024a49fdab69ea713ed13433
George Asiedu <83850864+george5-star@users.noreply.github.com>
2024-01-14 14:38:24 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,31 @@
-// Create a web server that can respond to requests for /comments.json with a 
-// JSON representation of the comments list used in your previous assignment.
+// Create a web server that can respond to requests for /comments.json
+// with a JSON file containing a list of comments. You’ll need to read
+// comments.json and convert it to a JavaScript object before responding
+// with it. Add an HTTP header of Access-Control-Allow-Origin: * to the
+// response.
 
-var http = require('http');
-var comments = require('./comments');
-var port = 8080;
+var http = require("http");
+var fs = require("fs");
 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(comments));
+var server = http.createServer(function(req, res) {
+  console.log("Request made for: " + req.url);
+  if (req.url === "/comments.json") {
+    fs.readFile("comments.json", function(err, data) {
+      if (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.end("Server error");
+        return;
+      }
+      res.setHeader("Content-Type", "application/json");
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.end(data);
+    });
+  } else {
+    res.statusCode = 404;
+    res.end("Not found");
+  }
 });
 
-server.listen(port);
-console.log('Server running on port ' + port);
+server.listen(8080);
+console.log("Server listening on port 8080");
